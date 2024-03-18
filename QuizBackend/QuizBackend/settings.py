bf99ad863b93643406ad7b2187591407225ecdb0
Aourl24 <awwalmustapha13@gmail.com>
2024-03-18 11:02:18 +0100

    'updated'

--- QuizBackend/QuizBackend/settings.py
+++ QuizBackend/QuizBackend/settings.py
@@ -132,6 +132,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000","http://192.168.133.92:3000","https://quizzifyapp.vercel.app/"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000","http://192.168.133.92:3000","https://quizzifyapp.vercel.app"]
 
 #CORS_ALLOW_ALL_ORIGINS = True
