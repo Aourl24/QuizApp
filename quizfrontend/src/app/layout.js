f82eac4d5dd2cea8e73f6deea099a3ca919418af
Aourl24 <awwalmustapha13@gmail.com>
2024-03-30 12:12:45 +0100

    'updated'

--- quizfrontend/src/app/layout.js
+++ quizfrontend/src/app/layout.js
@@ -17,7 +17,17 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="container">
        <h1 class="sz-20 mt-2"> Qui<span class="text-danger animate__animated animate__bounce animate__infinie animate__delay-3s" style={{display:'inline-block'}}>zz</span>ify </h1>
-      {children}</body>
+      <div class="row justify-content-evenly vh-100">
+      <div class="col-12"> {children} </div>
+
+      <div class="col">
+      <div class="row center">
+
+        <div class="col"> <span class="sz-14">LouLou</span> <br/> <i class="fas fa-phone"></i> +2349078025667, <i class="fab fa-google"></i> awwalmustapha@gmail.com </div>
+      </div>
+      </div>
+      </div>
+      </body>
     </html>
   )
 }
