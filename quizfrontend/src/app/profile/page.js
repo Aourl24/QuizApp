2964e8913bc650bfafe045193c8075cc06ef5e25
Aourl24 <awwalmustapha13@gmail.com>
2025-02-13 13:13:27 +0100

    'updated'

--- quizfrontend/src/app/profile/page.js
+++ quizfrontend/src/app/profile/page.js
@@ -1,34 +1,34 @@
-"use client"
-import React from "react"
-import {getData,postData} from '../endpoints.js'
-import {isAuthenticated,user} from '../auth.js'
+// "use client"
+// import React from "react"
+// import {getData,postData} from '../endpoints.js'
+// import {isAuthenticated,user} from '../auth.js'
 
-function App(props){
-	return(
-			<div>
-				{!isAuthenticated ? <Profile /> : <div class="center sz-18">You are not allowed to view this page</div>}
-			</div>
-		)
-}
+// function App(props){
+// 	return(
+// 			<div>
+// 				{!isAuthenticated ? <Profile /> : <div class="center sz-18">You are not allowed to view this page</div>}
+// 			</div>
+// 		)
+// }
 
-function Profile(props){
+// function Profile(props){
 
-const [profile,setProfile] = React.useState()
+// const [profile,setProfile] = React.useState()
 
-React.useEffect(()=>{
-	var resp = postData('profile',{user:user?.id}).then((res)=>setProfile(res.data))
-},[])
+// React.useEffect(()=>{
+// 	var resp = postData('profile',{user:user?.id}).then((res)=>setProfile(res.data))
+// },[])
 
-if(!profile){
-	return(<div class="w-100 center"><div class="spinner-border"></div></div>)
-}
+// if(!profile){
+// 	return(<div class="w-100 center"><div class="spinner-border"></div></div>)
+// }
 
-return(
-		<div>
-			Your Profile {profile.name}
-		</div>
-	)
-}
+// return(
+// 		<div>
+// 			Your Profile {profile.name}
+// 		</div>
+// 	)
+// }
 
 
-export default App;
+// export default App;
