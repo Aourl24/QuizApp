5df32c621844e018edd896780cd354389a55deb8
Aourl24 <awwalmustapha13@gmail.com>
2025-02-20 00:39:04 +0100

    'updated'

--- quizfrontend/src/app/endpoints.js
+++ quizfrontend/src/app/endpoints.js
@@ -3,9 +3,9 @@ import Cookies from 'js-cookie'
 
 // const api_host = '192.168.14.92:8000'
 // const api_host = '127.0.0.1:8000'
-const api_host = 'https://quizapp-p1lx.onrender.com'
-export const host = '127.0.0.1:3000'
-export const endpoint = `http://${api_host}/`//'http://127.0.0.1:8000/'
+const api_host = 'quizapp-p1lx.onrender.com'
+// export const host = '127.0.0.1:3000'
+export const endpoint = `https://${api_host}/`//'http://127.0.0.1:8000/'
 export const endpath = endpoint.trim().replace(/\/$/, '')
 export const wsEndpoint = `ws:/${api_host}/quizroom/`;
 axios.defaults.baseURL = endpoint
