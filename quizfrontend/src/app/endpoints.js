c6496287df67ff3f3757e2f5732c4eaf2689aa99
Aourl24 <awwalmustapha13@gmail.com>
2026-05-14 19:37:17 +0100

    uodated

--- quizfrontend/src/app/endpoints.js
+++ quizfrontend/src/app/endpoints.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 //const api_host = '127.0.0.1:8000'
-export const endpoint = 'https://quizzify.pythonanywhere.com/'
+const api_host = 'quizzify.pythonanywhere.com'
 
-//export const endpoint    = `http://${api_host}/`
+export const endpoint    = `https://${api_host}/`
 export const endpath     = endpoint.trim().replace(/\/$/, '')
 export const wsEndpoint  = `ws:/${api_host}/quizroom/`
 
